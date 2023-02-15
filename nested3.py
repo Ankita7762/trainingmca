74b1667efb5102ee61681c5ae4e32019338d8cf3
Ankita Singh <ankitasinghcsc@gmail.com>
2023-02-15 11:19:54 +0530

    level 2 (complete)

---
+++ nested3.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+speed('fastest')
+pencolor('red')
+fillcolor('yellow')
+for i in range(6):
+    fd(100)
+    for i in range(6):
+        fd(50)
+        for i in range(6):
+            fd(50)
+            rt(60)
+        end_fill()
+        lt(60)
+    rt(60)
+
+hideturtle()
+mainloop()
